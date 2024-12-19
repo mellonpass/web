cbe9eb4060e3ee7e92445113f883d67a265b124f
Roel Delos Reyes <badcoder28@gmail.com>
2024-12-20 03:19:33 +0800

    feat(signup): add static signup page.
    - add uikit css/js files

--- apps/signup/src/routes/+page.svelte
+++ apps/signup/src/routes/+page.svelte
@@ -1,1 +1,51 @@
-<h1>Welcome to your library project</h1>
+<section class="uk-height-viewport">
+    <header class="uk-container">
+        <div class="uk-navbar-left">
+            <a class="uk-navbar-item uk-logo" href="#" aria-label="Back to Home">MellonPass</a>
+        </div>
+    </header>
+    <div class="uk-flex uk-flex-center">
+        <section class="uk-width-1-3">
+            <header>
+                <h2 class="uk-text-center">Create account</h2>
+            </header>
+        
+            <form id="signup-form" action="" method="post">
+                <div class="uk-margin">
+                    <div class="uk-margin-small-bottom">
+                        <label for="name">Name</label>
+                    </div>
+                    <input class="uk-input" type="text" id="name" name="name" required>
+                </div>
+                <div class="uk-margin">
+                    <div class="uk-margin-small-bottom">
+                        <label for="email">Email</label>
+                    </div>
+                    <input class="uk-input" type="text" id="email" name="email" required>
+                </div>
+                <div class="uk-margin">
+                    <div class="uk-margin-small-bottom">
+                        <label for="password">Password</label>
+                    </div>
+                    <input class="uk-input" type="text" id="password" name="password" required>
+                </div>
+                <div class="uk-margin">
+                    <div class="uk-margin-small-bottom">
+                        <label for="confirm-password">Confirm password</label>
+                    </div>
+                    <input class="uk-input" type="text" id="confirm-password" name="confirm-password" required>
+                </div>
+
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-primary uk-width-1-1">Continue</button>
+                </div>
+            </form>
+        
+            <footer>
+                <div>
+                    By continuing, you agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+                </div>
+            </footer>
+        </section>
+    </div>
+</section>
