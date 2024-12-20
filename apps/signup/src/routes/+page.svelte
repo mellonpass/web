6684fd667e55b7edaff4810ed45827e8d7950694
Roel Delos Reyes <badcoder28@gmail.com>
2024-12-20 16:36:20 +0800

    feat(signup): add reactive validations

--- apps/signup/src/routes/+page.svelte
+++ apps/signup/src/routes/+page.svelte
@@ -1,3 +1,68 @@
+<script>
+    let matchedPasswords = $state(true);
+    const invalidityMapper = $state({});
+
+    const formFields = [
+        {
+            "id": "name",
+            "label": "Name",
+            "name": "name",
+            "type": "text",
+            "errorMsg": "Required field."
+        },
+        {
+            "id": "email",
+            "label": "Email",
+            "name": "email",
+            "type": "email",
+            "errorMsg": "Invalid email."
+        },
+        {
+            "id": "password",
+            "label": "Password",
+            "name": "password",
+            "type": "password",
+            "errorMsg": "Required field."
+        },
+        {
+            "id": "confirm-password",
+            "label": "Confirm password",
+            "name": "confirm-password",
+            "type": "password",
+            "errorMsg": "Required field."
+        },
+    ];
+
+
+    function matchPasswords(password1, password2) {
+        return password1 === password2;
+    }
+
+    async function handleSignUpSubmit(e) {
+        e.preventDefault();
+        const form = e.currentTarget;
+        const data = new FormData(form);
+
+        for (const [key, value] of data.entries()) {
+            const element = form.elements[key]
+            invalidityMapper[key] = !element.checkValidity();
+        }
+
+        if (Object.values(invalidityMapper).some(invalid => invalid)) {
+            return;
+        }
+
+        matchedPasswords = matchPasswords(form.elements["password"].value, form.elements["confirm-password"].value);
+    }
+
+    async function handleInputFocusOut(e) {
+        const element = e.currentTarget;
+        invalidityMapper[element.name] = !element.checkValidity();
+    }
+
+
+</script>
+
 <section class="uk-height-viewport">
     <header class="uk-container">
         <div class="uk-navbar-left">
@@ -9,35 +74,37 @@
             <header>
                 <h2 class="uk-text-center">Create account</h2>
             </header>
-        
-            <form id="signup-form" action="" method="post">
-                <div class="uk-margin">
-                    <div class="uk-margin-small-bottom">
-                        <label for="name">Name</label>
-                    </div>
-                    <input class="uk-input" type="text" id="name" name="name" required>
-                </div>
-                <div class="uk-margin">
-                    <div class="uk-margin-small-bottom">
-                        <label for="email">Email</label>
-                    </div>
-                    <input class="uk-input" type="text" id="email" name="email" required>
-                </div>
-                <div class="uk-margin">
-                    <div class="uk-margin-small-bottom">
-                        <label for="password">Password</label>
-                    </div>
-                    <input class="uk-input" type="text" id="password" name="password" required>
-                </div>
-                <div class="uk-margin">
-                    <div class="uk-margin-small-bottom">
-                        <label for="confirm-password">Confirm password</label>
-                    </div>
-                    <input class="uk-input" type="text" id="confirm-password" name="confirm-password" required>
-                </div>
 
+            <form id="signup-form" method="post" onsubmit={handleSignUpSubmit} novalidate>
                 <div class="uk-margin">
-                    <button class="uk-button uk-button-primary uk-width-1-1">Continue</button>
+                    {#each formFields as formField (formField.id)}
+                        <div class="uk-margin-small-bottom">
+                            <label for="{formField.name}">{formField.label}</label>
+                        </div>
+                        <input
+                            onfocusout={handleInputFocusOut}
+                            class:uk-form-danger={invalidityMapper[formField.name]}
+                            class="uk-input"
+                            type="{formField.type}"
+                            id="{formField.id}"
+                            name="{formField.name}"
+                            required
+                        >
+                        {#if invalidityMapper[formField.name]}
+                            <div class="uk-margin-small uk-text-default uk-text-danger">
+                                {formField.errorMsg}
+                            </div>
+                        {/if}
+                    {/each}
+
+                    {#if !matchedPasswords}
+                        <div class="uk-margin-small uk-text-default uk-text-danger">
+                            Password mismatched!
+                        </div>
+                    {/if}
+                    <div class="uk-margin">
+                        <button class="uk-button uk-button-primary uk-width-1-1">Continue</button>
+                    </div>
                 </div>
             </form>
         
