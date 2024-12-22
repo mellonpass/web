dffceac8ecb843a85697dca2a8553c20d4a5b314
Roel Delos Reyes <badcoder28@gmail.com>
2024-12-23 02:13:05 +0800

    feat(signup): add successful sign up indicator and instruction.

--- apps/web/src/routes/sign-up/+page.svelte
+++ apps/web/src/routes/sign-up/+page.svelte
@@ -2,6 +2,7 @@
     import { checkEmailIfExists, createAccount } from '../../lib/accounts';
 
     let matchedPasswords = $state(true);
+    let signUpSuccessful = $state(false);
 
     const serverErrors = $state([]);
     const invalidityMapper = $state({});
@@ -10,7 +11,7 @@
             "id": "name",
             "label": "Name",
             "name": "name",
-            "value": "",
+            "value": null,
             "type": "text",
             "errorMsg": "Required field.",
         },
@@ -18,7 +19,7 @@
             "id": "email",
             "label": "Email",
             "name": "email",
-            "value": "",
+            "value": null,
             "type": "email",
             "errorMsg": "Invalid email.",
         },
@@ -70,6 +71,7 @@
         if (validateName()) {
             try {
                 await createAccount(nameField.value, emailField.value);
+                signUpSuccessful = true;
             } catch (error) {
                 serverErrors.length = 0;
                 // if server returns a multiple error validation.
@@ -116,52 +118,66 @@
     </header>
     <div class="uk-flex uk-flex-center">
         <section class="uk-width-1-3">
-            <header>
-                <h2 class="uk-text-center">Create account</h2>
-            </header>
+            {#if !signUpSuccessful}
+                <header>
+                    <h2 class="uk-text-center">Create account</h2>
+                </header>
 
 
-            {#each serverErrors as error (error)}
-                <div class="uk-alert-danger" uk-alert>
-                    <a href class="uk-alert-close" uk-close></a>
-                    <p>{error.message}</p>
-                </div>
-            {/each}
-
-            <form id="signup-form" method="post" onsubmit={handleSignUpSubmit} novalidate>
-                <div class="uk-margin">
-                    {#each formFields as formField (formField.id)}
-                        <div class="uk-margin-small">
-                            <label for="{formField.name}">{formField.label}</label>
-                        </div>
-                        <input
-                            onfocusout={handleInputFocusOut}
-                            class:uk-form-danger={invalidityMapper[formField.name]}
-                            class="uk-input"
-                            bind:value={formField.value}
-                            type="{formField.type}"
-                            id="{formField.id}"
-                            name="{formField.name}"
-                            required
-                        >
-                        {#if invalidityMapper[formField.name]}
-                            <div class="uk-margin-small uk-text-default uk-text-danger">
-                                {formField.errorMsg}
-                            </div>
-                        {/if}
-                    {/each}
-
-                    <div class="uk-margin">
-                        <button class="uk-button uk-button-primary uk-width-1-1">Continue</button>
+                {#each serverErrors as error (error)}
+                    <div class="uk-alert-danger" uk-alert>
+                        <a href class="uk-alert-close" uk-close></a>
+                        <p>{error.message}</p>
                     </div>
-                </div>
-            </form>
-        
-            <footer>
-                <div>
+                {/each}
+
+                <form id="signup-form" method="post" onsubmit={handleSignUpSubmit} novalidate>
+                    <div class="uk-margin">
+                        {#each formFields as formField (formField.id)}
+                            <div class="uk-margin-small">
+                                <label for="{formField.name}">{formField.label}</label>
+                            </div>
+                            <input
+                                onfocusout={handleInputFocusOut}
+                                class:uk-form-danger={invalidityMapper[formField.name]}
+                                class="uk-input"
+                                bind:value={formField.value}
+                                type="{formField.type}"
+                                id="{formField.id}"
+                                name="{formField.name}"
+                                required
+                            >
+                            {#if invalidityMapper[formField.name]}
+                                <div class="uk-margin-small uk-text-default uk-text-danger">
+                                    {formField.errorMsg}
+                                </div>
+                            {/if}
+                        {/each}
+
+                        <div class="uk-margin">
+                            <button class="uk-button uk-button-primary uk-width-1-1">Continue</button>
+                        </div>
+                    </div>
+                </form>
+                
+                <p>
                     By continuing, you agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
+                </p>
+            {:else}
+                <header>
+                    <h2 class="uk-text-center">Check your email</h2>
+                </header>
+
+                <div class="uk-card uk-card-medium uk-card-default uk-card-body">
+                    <p class="uk-text-center">
+                        Follow the link in the email sent to <strong>{emailField.value}</strong> and continue creating your account.
+                    </p>
                 </div>
-            </footer>
+
+                <p class="uk-text-center">
+                    Already have an account? <a href="#">Log in</a>.
+                </p>
+            {/if}
         </section>
     </div>
 </section>
