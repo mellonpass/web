0e8702dc751626fe17dc1bc4e3f90159ef3d7f6e
Roel Delos Reyes <badcoder28@gmail.com>
2025-01-06 08:41:35 +0800

    feat(crypto): update generateMasterKey function to return two master key types, one for login hash and one for HKDF.

--- apps/web/src/lib/crypto/index.js
+++ apps/web/src/lib/crypto/index.js
@@ -1,3 +1,11 @@
+/**
+ * Generates two PBKDF2 master keys from the user's email and master password.
+ *  - The PBKDF2 master key to generate a HFKD key.
+ *  - The PBKDF2 master ket to generate a login hash.
+ * @param {string} email - The verified email of the user.
+ * @param {string} masterPassword - The master password in plain text.
+ * @returns {Object} - The PBKDF2 and the HKDF master keys.
+ */
 export const generateMasterKey = async (email, masterPassword) => {
     const encoder = new TextEncoder();
     const salt = encoder.encode(email);
@@ -17,5 +25,11 @@ export const generateMasterKey = async (email, masterPassword) => {
     );
     const derivedBits = await crypto.subtle.deriveBits(algorithm, baseKey, 256)
 
-    return await crypto.subtle.importKey("raw", derivedBits, "HKDF", false, ["deriveKey"]);
+    const pbkdf2ForLoginHash = await crypto.subtle.importKey("raw", derivedBits, "PBKDF2", false, ["deriveKey"]);
+    const pbkdf2ForHKDF = await crypto.subtle.importKey("raw", derivedBits, "HKDF", false, ["deriveKey"]);
+
+    return {
+        masterKeyForLoginHash: pbkdf2ForLoginHash,
+        masterKeyForHKDF: pbkdf2ForHKDF,
+    }
 };
