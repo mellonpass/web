f7e6106a219a7e46d38077bd30a85abf31f4c683
Roel Delos Reyes <badcoder28@gmail.com>
2025-01-25 23:14:59 +0800

    feat(new-item): secure note form

--- apps/web/src/components/Vault/types/SecureNote/VaultSecureNoteAdd.svelte
+++ apps/web/src/components/Vault/types/SecureNote/VaultSecureNoteAdd.svelte
@@ -1,1 +1,28 @@
-<div class="uk-modal-body">Secure note</div>
+
+<div class="uk-modal-body">
+    <form class="uk-form-stacked">
+        <div class="uk-margin">
+            <!-- svelte-ignore a11y_autofocus -->
+            <input
+                value="Secure Note"
+                type="text"
+                aria-label="Item title"
+                class="uk-input uk-form-large uk-border-rounded"
+                autofocus
+            >
+        </div>
+
+        <div class="uk-margin">
+            <div class="uk-form-controls">
+                <textarea style="resize: none;" class="uk-textarea uk-border-rounded" aria-label="Textarea"></textarea>
+            </div>
+        </div>
+    </form>
+</div>
+
+<div class="uk-modal-footer uk-flex uk-flex-row-reverse">
+    <div class="uk-margin">
+        <button class="uk-button uk-button-primary">Save</button>
+        <button onclick={() => {UIkit.modal("#vault-modal").hide()}}  class="uk-button uk-button-default">Cancel</button>
+    </div>
+</div>
