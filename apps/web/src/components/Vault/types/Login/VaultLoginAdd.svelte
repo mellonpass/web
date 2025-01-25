830c6c1e9f237989498ac2e5d8513b7e1255ff8f
Roel Delos Reyes <badcoder28@gmail.com>
2025-01-25 23:14:49 +0800

    feat(new-item): login form

--- apps/web/src/components/Vault/types/Login/VaultLoginAdd.svelte
+++ apps/web/src/components/Vault/types/Login/VaultLoginAdd.svelte
@@ -1,1 +1,48 @@
-<div class="uk-modal-body">Login</div>
+<script>
+    let passwordToggle = $state(false);
+</script>
+
+<div class="uk-modal-body">
+    <form class="uk-form-stacked">
+        <div class="uk-margin">
+            <!-- svelte-ignore a11y_autofocus -->
+            <input
+                value="Login"
+                type="text"
+                aria-label="Item title"
+                class="uk-input uk-form-large uk-border-rounded"
+                autofocus
+            >
+        </div>
+
+        <div class="uk-margin">
+            <label class="uk-form-label" for="form-stacked-text">Username</label>
+            <div class="uk-form-controls">
+                <input class="uk-input uk-border-rounded" type="text">
+            </div>
+        </div>
+        <div class="uk-margin">
+            <label class="uk-form-label" for="form-stacked-select">Password</label>
+            <div class="uk-form-controls">
+                <div class="uk-inline uk-width-100">
+                    <input class="uk-input uk-border-rounded" type="{passwordToggle ? 'text' : 'password'}">
+                    <a
+                        aria-label="eye-icon"
+                        class="uk-form-icon uk-form-icon-flip"
+                        href
+                        uk-icon="icon: {passwordToggle ? 'eye-slash' : 'eye'}"
+                        onclick={() => {passwordToggle = !passwordToggle}}
+                    >
+                    </a>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+<div class="uk-modal-footer uk-flex uk-flex-row-reverse">
+    <div class="uk-margin">
+        <button class="uk-button uk-button-primary">Save</button>
+        <button onclick={() => {UIkit.modal("#vault-modal").hide()}}  class="uk-button uk-button-default">Cancel</button>
+    </div>
+</div>
