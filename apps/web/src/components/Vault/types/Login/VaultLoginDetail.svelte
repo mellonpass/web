11a1ebd4bdfaee557c47d2f522f8c3e06b6a92b4
Roel Delos Reyes <badcoder28@gmail.com>
2025-02-22 00:45:59 +0800

    fix: properly align copied to clipboard information element.

--- apps/web/src/components/Vault/types/Login/VaultLoginDetail.svelte
+++ apps/web/src/components/Vault/types/Login/VaultLoginDetail.svelte
@@ -15,38 +15,41 @@
 
 </script>
 
-<div class="uk-panel x-panel-container">
-    <div class="uk-padding-small">
-        { /* @ts-ignore */ null }
-        <div class="uk-grid-small uk-flex-middle" uk-grid>
-            <div class="uk-width-auto">
-                <img class="uk-border-round" width="60" height="60" src="https://placehold.jp/150x150.png" alt="Avatar">
-            </div>
-            <div class="uk-width-expand">
-                <h3 class="uk-card-title uk-margin-remove-bottom">{cipher.name}</h3>
+
+<div class="uk-flex uk-flex-column uk-flex-between uk-height-1-1">
+    <div class="uk-panel x-panel-container">
+        <div class="uk-padding-small">
+            { /* @ts-ignore */ null }
+            <div class="uk-grid-small uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                    <img class="uk-border-round" width="60" height="60" src="https://placehold.jp/150x150.png" alt="Avatar">
+                </div>
+                <div class="uk-width-expand">
+                    <h3 class="uk-card-title uk-margin-remove-bottom">{cipher.name}</h3>
+                </div>
             </div>
         </div>
+        <div class="x-panel uk-border-rounded">
+            <a onclick={(e) => {onCopy(cipher.data.username)}} href={null} class="x-login-item uk-padding-small uk-flex uk-text-decoration-none">
+                <span class="uk-text-muted uk-margin-right">Username: </span>
+                <span class="uk-text-emphasis uk-text-baseline uk-width-expand">{cipher.data.username}</span>
+                <span class="x-copy-label uk-text-right uk-text-decoration-none">COPY</span>
+            </a>
+            <hr class="uk-margin-remove">
+            <a onclick={(e) => {onCopy(cipher.data.password)}} href={null} class="x-login-item uk-padding-small uk-flex uk-text-decoration-none">
+                <span class="uk-text-muted uk-margin-right">Password: </span>
+                <span class="uk-text-emphasis uk-text-baseline uk-width-expand">• • • • • • • • • •</span>
+                <span class="x-copy-label uk-text-right uk-text-decoration-none">COPY</span>
+            </a>
+        </div>
     </div>
-    <div class="x-panel uk-border-rounded">
-        <a onclick={(e) => {onCopy(cipher.data.username)}} href={null} class="x-login-item uk-padding-small uk-flex uk-text-decoration-none">
-            <span class="uk-text-muted uk-margin-right">Username: </span>
-            <span class="uk-text-emphasis uk-text-baseline uk-width-expand">{cipher.data.username}</span>
-            <span class="x-copy-label uk-text-right uk-text-decoration-none">COPY</span>
-        </a>
-        <hr class="uk-margin-remove">
-        <a onclick={(e) => {onCopy(cipher.data.password)}} href={null} class="x-login-item uk-padding-small uk-flex uk-text-decoration-none">
-            <span class="uk-text-muted uk-margin-right">Password: </span>
-            <span class="uk-text-emphasis uk-text-baseline uk-width-expand">• • • • • • • • • •</span>
-            <span class="x-copy-label uk-text-right uk-text-decoration-none">COPY</span>
-        </a>
-    </div>
-</div>
 
-{#if copied}
-    <div in:fade={{ duration: 50 }} class="x-copy-info uk-margin">
-        Copied to clipboard.
-    </div>
-{/if}
+    {#if copied}
+        <div in:fade={{ duration: 50 }} class="x-copy-info uk-margin">
+            Copied to clipboard.
+        </div>
+    {/if}
+</div>
 
 <style>
     .x-panel {
@@ -57,7 +60,7 @@
     .x-login-item:hover {
         cursor: pointer;
         font-weight: 500;
-        background: #E3F2FD;
+        background: #eff4fd;
     }
 
     .x-copy-label {
