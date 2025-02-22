c423b86afb2a47b6b3ef1f369c3788cef527d131
Roel Delos Reyes <badcoder28@gmail.com>
2025-02-22 19:40:59 +0800

    feat(cipher update): implement cipher update service on vault content.
    - handle error response.
    - handle success response.
    - reload cipher details.

--- apps/web/src/components/Vault/VaultContent.svelte
+++ apps/web/src/components/Vault/VaultContent.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
+
+    import { getContext, onMount } from "svelte";
+    import { fade } from "svelte/transition";
+
     import VaultLoginDetail from "$components/Vault/types/Login/VaultLoginDetail.svelte";
     import VaultLoginEdit from "$components/Vault/types/Login/VaultLoginEdit.svelte";
-
     import VaultSecureNoteEdit from "$components/Vault/types/SecureNote//VaultSecureNoteEdit.svelte";
     import VaultSecureNoteDetail from "$components/Vault/types/SecureNote/VaultSecureNoteDetail.svelte";
+
     import { extractSymmetricKey, generateCipherKey } from "$lib/key-generation";
+    import { updateCipher } from "$lib/services/ciphers";
     import { cipherStore } from "$lib/stores";
     import { decryptCipher, encryptCipher } from "$lib/symmetric-encryption";
-
     import { CipherType, type Cipher } from "$lib/types";
-    import { getContext, onMount } from "svelte";
 
     let { vaultId = null } = $props();
 
@@ -31,7 +34,7 @@
     let componentData: any = $state({});
 
     let editMode = $state(false);
-    let formErrors = $state([]);
+    let formErrors: Array<string> = $state([]);
     let cipher: Cipher | null = $state(null);
 
     let VaultComponent = $derived.by(() => {
@@ -61,7 +64,8 @@
 
         const ck = await generateCipherKey();
         const sk = await extractSymmetricKey(mk, epsk);
-        let cipher = null;
+
+        let _cipher: Partial<Cipher> | null = null;
 
         let baseCipherData = {
             sk: sk,
@@ -72,7 +76,7 @@
 
         switch (componentData.type) {
             case CipherType.LOGIN:
-                cipher = await encryptCipher({
+                _cipher = await encryptCipher({
                     ...baseCipherData,
                     data: {
                         username: componentData.username,
@@ -81,7 +85,7 @@
                 });
                 break;
             case CipherType.SECURE_NOTE:
-                cipher = await encryptCipher({
+                _cipher = await encryptCipher({
                     ...baseCipherData,
                     data: {
                         note: componentData.note,
@@ -90,7 +94,20 @@
                 break;
         }
 
-        console.log(cipher);
+        const cipherInput: Cipher = {..._cipher, id: vaultId} as Cipher;
+
+        const response = await updateCipher(cipherInput!);
+        switch (response.data.cipher.update.__typename) {
+            case "Cipher":
+                const updatedCipher = response.data.cipher.update as Cipher;
+                cipherStore.edit(updatedCipher);
+                loadCipherDetail();
+                editMode = !editMode;
+                break;
+            default:
+                formErrors.push(response.data.cipher.update.message);
+                break;
+        }
     };
 
     onMount(async () => {
@@ -110,14 +127,14 @@
                     <button form="vault-form" class="uk-button uk-button-primary uk-button-small uk-border-rounded">
                         Save
                     </button>
-                    <button class="uk-button uk-button-default uk-button-small uk-border-rounded" onclick={() => {editMode = !editMode}}>
+                    <button class="uk-button uk-button-default uk-button-small uk-border-rounded" onclick={() => {editMode = !editMode; formErrors = [];}}>
                         Cancel
                     </button>
                 </div>
             </div>
         {:else}
             <div class="uk-flex uk-flex-right">
-                <button onclick={() => {editMode = !editMode}} class="uk-button uk-button-default uk-button-small uk-border-rounded">
+                <button onclick={() => {editMode = !editMode; formErrors = [];}} class="uk-button uk-button-default uk-button-small uk-border-rounded">
                    Edit
                 </button>
                 <div class="uk-inline x-vertical-center">
@@ -171,7 +188,7 @@
             novalidate
         >
             {#if formErrors.length > 0}
-                <div class="uk-padding-small uk-text-small">
+                <div transition:fade={{ duration: 100 }} class="uk-padding-small uk-text-small">
                     { /* @ts-ignore */ null }
                     <div class="uk-alert-danger" uk-alert>
                         <ul>
