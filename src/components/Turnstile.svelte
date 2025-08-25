5ff432f82fd01030d623141d8ee9b799ad15767f
Roel Delos Reyes <badcoder28@gmail.com>
2025-08-25 22:07:57 +0800

    feat(cf): add turnstile validation on sign up account component.

--- src/components/Turnstile.svelte
+++ src/components/Turnstile.svelte
@@ -20,7 +20,7 @@
     let {
         sitekey,
         action,
-        execution = "execute",
+        execution = "render",
         size = "flexible",
         retry = "auto",
         retryInterval = 8000,
