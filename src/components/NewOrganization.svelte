fb1ffe4fcae3661f38c2dcb2f8f1024285c12d4e
Nikunj kumar <nikunjkumar1062@gmail.com>
2025-09-11 14:41:44 +0530

    UI: added New Organization modal and updated layout

---
+++ src/components/NewOrganization.svelte
@@ -0,0 +1,51 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+  let orgName = "";
+  let showModal = false;
+
+  function openModal() {
+    showModal = true;
+  }
+
+  function closeModal() {
+    showModal = false;
+  }
+
+  function createOrganization() {
+    if (orgName.trim()) {
+      dispatch("create", { name: orgName });
+      orgName = "";
+      closeModal();
+    }
+  }
+</script>
+
+<!-- Button to open modal -->
+<button class="uk-button uk-button-primary" on:click={openModal}>
+  New Organization
+</button>
+
+<!-- Modal -->
+{#if showModal}
+  <div class="uk-modal uk-open" style="display: block; background: rgba(0,0,0,0.5);">
+    <div class="uk-modal-dialog uk-modal-body uk-margin-auto-vertical">
+      <h3 class="uk-modal-title">Create New Organization</h3>
+      <input
+        class="uk-input uk-margin-small-top"
+        placeholder="Enter organization name"
+        bind:value={orgName}
+      />
+
+      <div class="uk-margin-top uk-flex uk-flex-right">
+        <button class="uk-button uk-button-default" on:click={closeModal}>
+          Cancel
+        </button>
+        <button class="uk-button uk-button-primary uk-margin-small-left" on:click={createOrganization}>
+          Create
+        </button>
+      </div>
+    </div>
+  </div>
+{/if}
--- src/routes/(protected)/+layout.svelte
+++ src/routes/(protected)/+layout.svelte
@@ -2,6 +2,13 @@
 	let { children } = $props();
 </script>
 
+<script>
+  import NewOrganization from "../components/NewOrganization.svelte";
+</script>
+<nav>
+  <!-- Other menu items -->
+  <NewOrganization on:create={(e) => console.log("Organization created:", e.detail)} />
+</nav>
 
 <section class="uk-height-viewport">
     {@render children()}
