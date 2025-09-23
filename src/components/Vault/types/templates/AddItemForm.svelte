97bdc576e655c94990fece0636c01ec8ac9c9e54
Roel Delos Reyes <badcoder28@gmail.com>
2025-09-24 00:26:01 +0800

    feat(AddItemForm): bind data to important input controls.
    - handle invalid fields with error indicator and focus.

--- src/components/Vault/types/templates/AddItemForm.svelte
+++ src/components/Vault/types/templates/AddItemForm.svelte
@@ -1,8 +1,44 @@
 <script lang="ts">
-    let { title, children } = $props();
+    type FormItemDetails = {
+        name: string
+    }
+    type FormAdditionalOptions = {
+        notes: string
+    };
+    type FormCallBack  = (args: any) => void;
+
+    let {
+        id,
+        title,
+        itemDetails,
+        additionalOptions,
+        onSubmit,
+        children
+    }: {
+        id: string,
+        title: string,
+        itemDetails: FormItemDetails,
+        additionalOptions: FormAdditionalOptions,
+        onSubmit: FormCallBack,
+        children: any
+    } = $props();
+
+    let invalidInput = $state(false);
+
+    const handleInput = (e: any) => {
+        const el = e.target;
+        invalidInput = false;
+    };
+
+    const handleInvalid = (e: any) => {
+        e.preventDefault();
+        invalidInput = true;
+        e.target.focus();
+    };
+
 </script>
 
-<form class="uk-form-stacked">
+<form id="{id}" onsubmit={onSubmit} class="uk-form-stacked">
     <!-- Item details -->
     <fieldset class="uk-fieldset uk-margin">
          <legend class="uk-legend uk-text-default uk-text-bold">Item details</legend>
@@ -11,11 +47,19 @@
             <input
                 type="text"
                 aria-label="Item title"
-                class="uk-input uk-border-rounded"
+                class="uk-input uk-border-rounded {invalidInput ? 'uk-form-danger' : ''}"
                 placeholder="Item name"
+                bind:value={itemDetails.name}
+                oninput={handleInput}
+                oninvalid={handleInvalid}
                 required
                 autofocus
             >
+            {#if invalidInput}
+                <div class="uk-margin-small uk-text-meta uk-text-danger">
+                    This field is required.
+                </div>
+            {/if}
         </div>
     </fieldset>
 
@@ -32,11 +76,14 @@
          <div class="uk-margin-small">
             <div class="uk-form-controls">
                 <textarea
-                    rows=10
                     style="resize: none;"
                     class="uk-textarea uk-border-rounded"
                     aria-label="Textarea"
-                    required></textarea>
+                    placeholder="Notes"
+                    rows=5
+                    bind:value={additionalOptions.notes}
+                >
+                </textarea>
             </div>
         </div>
     </fieldset>
