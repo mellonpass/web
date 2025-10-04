7c10d47d194540c2a0a31013a241c3b87a56056d
Roel Delos Reyes <badcoder28@gmail.com>
2025-10-04 23:26:48 +0800

    feat(vault item card): update vault card detail to have a modular class for reusable field renderer.

---
+++ src/components/Vault/types/Card/VaultCardDetail.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import vaultImage from "$lib/assets/images/vaultImage.png";
+    import { VaultCardDetailComponentData } from "$lib/models/data";
+
+    let { cipher } = $props();
+    const data = new VaultCardDetailComponentData(cipher.data);
+    const fields = data.getFields();
+
+</script>
+
+<div class="uk-panel">
+    <div class="uk-padding-small">
+        { /* @ts-ignore */ null}
+        <div class="uk-grid-small uk-flex-middle" uk-grid>
+            <div class="uk-width-auto">
+                <img class="uk-border-round" width="60" height="60" src={vaultImage} alt="Avatar">
+            </div>
+            <div class="uk-width-expand">
+                <h3 class="uk-card-title uk-margin-remove-bottom">{cipher.name}</h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="uk-padding-small">
+        <div class="uk-margin-small uk-margin-xsmall-left uk-text-small uk-text-bold">{cipher.data.brand || "Card"} details</div>
+        <div class="x-panel uk-width-1-1 uk-padding-small">
+            {#each fields as field, i}
+                {#if field.value && !field.metadata?.hidden}
+                    <div class="uk-margin" class:uk-margin-remove-bottom={i === fields.length - 1 || i === 0}>
+                        <div class="uk-text-meta">{field.label}</div>
+                        <div>{field.value}</div>
+                        <hr class="uk-margin-remove">
+                    </div>
+                {/if}
+            {/each}
+        </div>
+    </div>
+</div>
+
+<style>
+    .x-panel {
+        background-color: white;
+        border: solid 1px whitesmoke;
+        border-radius: 10px;
+    }
+</style>
--- src/lib/models/data.ts
+++ src/lib/models/data.ts
@@ -1,3 +1,5 @@
+import type { CipherCardData, CipherData } from "../types";
+
 export class ProtectedData {
   data: Uint8Array<ArrayBuffer>;
   mac: Uint8Array<ArrayBuffer>;
@@ -10,5 +12,121 @@ export class ProtectedData {
     this.iv = databuffer.slice(IV_START_LEN, databuffer.byteLength);
     this.mac = databuffer.slice(MAC_START_LEN, IV_START_LEN);
     this.data = databuffer.slice(0, MAC_START_LEN);
+  }
+}
+
+interface VaultDetailField {
+  value: any | null;
+  label: string;
+  type: string;
+  metadata?: { [key: string]: any };
+}
+
+interface VaultDetailFields {
+  [key: string]: VaultDetailField;
+}
+
+abstract class VaultDetailComponentData<T extends CipherData> {
+  protected data: T;
+
+  fields: VaultDetailFields | null = null;
+
+  constructor(data: T) {
+    this.data = data;
+  }
+
+  protected abstract fieldDefinitions(): VaultDetailFields;
+
+  private evaluateValueByType(value: any, type: string) {
+    switch (type) {
+      case "password":
+        return "*****";
+      default:
+        return value;
+    }
+  }
+
+  getFields(): Array<VaultDetailField> {
+    this.fields = this.fieldDefinitions();
+
+    const fieldNames = Object.keys(this.fields) as Array<
+      keyof VaultDetailFields
+    >;
+
+    const fields_: Array<VaultDetailField> = [];
+    fieldNames.forEach((key) => {
+      if (this.fields) {
+        const field = this.fields[key];
+        field.value = this.evaluateValueByType(field.value, field.type);
+        fields_.push(field);
+      }
+    });
+
+    return fields_;
+  }
+}
+
+export class VaultCardDetailComponentData extends VaultDetailComponentData<CipherCardData> {
+  constructor(data: CipherCardData) {
+    super(data);
+  }
+
+  protected fieldDefinitions() {
+    const fields: VaultDetailFields = {
+      name: {
+        value: this.data.name,
+        label: "Cardholder name",
+        type: "text",
+      },
+      number: {
+        value: this.data.number,
+        label: "Number",
+        type: "password",
+      },
+      brand: {
+        value: this.data.brand,
+        label: "Brand",
+        type: "text",
+        metadata: { hidden: true },
+      },
+    };
+
+    if (this.data.expMonth || this.data.expYear) {
+      // Nothing to worry reaching year 3000.
+      const currentMillennia = 2000;
+      const month = this.data.expMonth || "__";
+      let year = this.data.expYear || "____";
+
+      if (year.length > 4) {
+        const currentYear = new Date().getFullYear();
+        const currentYearWithinMillenia = currentYear - currentMillennia;
+
+        // Get the last x digit of the provided year
+        // where x is the char length of currentYearWithinMillenia.
+        const calculatedYearWithinMillenia = year
+          .toString()
+          .slice(-String(currentYearWithinMillenia).length);
+
+        year = (
+          currentMillennia + parseInt(calculatedYearWithinMillenia)
+        ).toString();
+      } else if (year.length < 4) {
+        year = (currentMillennia + parseInt(year)).toString();
+      }
+
+      fields.expiration = {
+        value: `${month}/${year}`,
+        label: "Expiration",
+        type: "datetime",
+      };
+    }
+
+    fields.securityCode = {
+      value: this.data.securityCode,
+      label: "Security Code",
+      type: "password",
+    };
+
+    return fields;
   }
 }
