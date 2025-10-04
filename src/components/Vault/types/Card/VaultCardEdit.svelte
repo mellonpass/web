a220a1f4d8dc051bf18d9d8f433299f0b3fe37c6
Roel Delos Reyes <badcoder28@gmail.com>
2025-10-04 23:27:10 +0800

    feat(vault item card): initial page for vault card edit.

---
+++ src/components/Vault/types/Card/VaultCardEdit.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { writable } from 'svelte/store';
+
+    import vaultImage from "$lib/assets/images/vaultImage.png";
+
+    let { cipher, data = $bindable({}) } = $props();
+
+    const initData = writable({
+        id: cipher.id,
+        type: cipher.type,
+        name: cipher.name,
+        data: {
+            note: cipher.data.note,
+        }
+    });
+
+</script>
+
+<div class="uk-panel">
+    <div class="uk-padding-small">
+        { /* @ts-ignore */ null}
+        <div class="uk-grid-small uk-flex-middle" uk-grid>
+            <div class="uk-width-auto">
+                <img class="uk-border-round" width="60" height="60" src={vaultImage} alt="Avatar">
+            </div>
+            <div class="uk-width-expand">
+               
+            </div>
+        </div>
+    </div>
+
+</div>
