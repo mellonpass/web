34219971b03d3a09889e61a9e51a22f21c2a8c3f
Roel Delos Reyes <badcoder28@gmail.com>
2025-10-09 11:50:37 +0800

    chore(vault item card): move ProtectedData on keys.ts model.

--- src/lib/models/data.ts
+++ src/lib/models/data.ts
@@ -1,19 +1,4 @@
-import type { Cipher, CipherCardData, CipherData } from "../types";
-
-export class ProtectedData {
-  data: Uint8Array<ArrayBuffer>;
-  mac: Uint8Array<ArrayBuffer>;
-  iv: Uint8Array<ArrayBuffer>;
-
-  constructor(databuffer: Uint8Array<ArrayBuffer>) {
-    const IV_START_LEN = databuffer.length - 16;
-    const MAC_START_LEN = IV_START_LEN - 32;
-
-    this.iv = databuffer.slice(IV_START_LEN, databuffer.byteLength);
-    this.mac = databuffer.slice(MAC_START_LEN, IV_START_LEN);
-    this.data = databuffer.slice(0, MAC_START_LEN);
-  }
-}
+import type { CipherCardData, CipherData } from "../types";
 
 const CLIPBOARD_CLEAR_DELAY = 1000 * 30; // 30 seconds
 
--- src/lib/models/keys.ts
+++ src/lib/models/keys.ts
@@ -1,7 +1,24 @@
 import { arrayBufferToHex, hexToArrayBuffer } from "$lib/bytes";
-import { ProtectedData } from "./data";
 
 const BIT_SIZE = 8;
+
+/**
+ * Encrypted and signed cipher data.
+ */
+export class ProtectedData {
+  data: Uint8Array<ArrayBuffer>;
+  mac: Uint8Array<ArrayBuffer>;
+  iv: Uint8Array<ArrayBuffer>;
+
+  constructor(databuffer: Uint8Array<ArrayBuffer>) {
+    const IV_START_LEN = databuffer.length - 16;
+    const MAC_START_LEN = IV_START_LEN - 32;
+
+    this.iv = databuffer.slice(IV_START_LEN, databuffer.byteLength);
+    this.mac = databuffer.slice(MAC_START_LEN, IV_START_LEN);
+    this.data = databuffer.slice(0, MAC_START_LEN);
+  }
+}
 
 /**
  * Base key that imports AES and MAC keys from the keybuffer.
