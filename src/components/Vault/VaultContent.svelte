906bcf9dac6694b731f0bbfd5b8d571e6ec4e28e
Roel Delos Reyes <badcoder28@gmail.com>
2025-10-18 13:58:59 +0800

    docs: minor comments.

--- src/components/Vault/VaultContent.svelte
+++ src/components/Vault/VaultContent.svelte
@@ -68,7 +68,7 @@
 
         const cipher = await encryptVaultDetailForUpdate({vaultDetail: vaultItemDetail!, sk: sk!})
 
-        // Refactor with hashmap.
+        // FIXME: Refactor with hashmap.
         switch (status) {
             case VaultStatus.ACTIVE:
                 // Check if current vault content data status is either archived or deleted.
